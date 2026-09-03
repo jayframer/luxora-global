5beab449e9315510571374c11b2172b9504a18bd
Jaysinh Parmar <jay.framerr@gmail.com>
2026-09-03 23:00:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -246,8 +246,8 @@ app.get('/api/inquiries/export', (req, res) => {
   }
 });
 
-const DIST_DIR = join(__dirname, '..', 'dist');
-const PUBLIC_DIR = join(__dirname, '..', 'public');
+const DIST_DIR = join(process.cwd(), 'dist');
+const PUBLIC_DIR = join(process.cwd(), 'public');
 
 app.use(express.static(DIST_DIR));
 app.use('/logo.png', express.static(join(__dirname, 'logo.png')));
@@ -257,11 +257,23 @@ app.get('/googlee9ec779f9ed407e4.html', (req, res) => {
 });
 
 app.get('/sitemap.xml', (req, res) => {
-  res.sendFile(join(PUBLIC_DIR, 'sitemap.xml'));
+  const distPath = join(DIST_DIR, 'sitemap.xml');
+  const publicPath = join(PUBLIC_DIR, 'sitemap.xml');
+  if (existsSync(distPath)) {
+    res.sendFile(distPath);
+  } else {
+    res.sendFile(publicPath);
+  }
 });
 
 app.get('/robots.txt', (req, res) => {
-  res.sendFile(join(PUBLIC_DIR, 'robots.txt'));
+  const distPath = join(DIST_DIR, 'robots.txt');
+  const publicPath = join(PUBLIC_DIR, 'robots.txt');
+  if (existsSync(distPath)) {
+    res.sendFile(distPath);
+  } else {
+    res.sendFile(publicPath);
+  }
 });
 
 app.get('/admin', (req, res) => {
